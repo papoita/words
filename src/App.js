78a439ee8cc7db252f2d3bad35d653ee23131545
PaolaP <paolaperez8@gmail.com>
2022-07-06 15:51:47 -0400

    add json for db

--- src/App.js
+++ src/App.js
@@ -2,9 +2,54 @@
 export default function App() {
   return (
     <div className="App">
-      <h1> Words </h1>
+      <h1> Food Words </h1>
     </div>
   );
 }
 
 
+/*
+SOLUTION
+5 letter string db, eg. "drain"
+
+PAST GUESSES 
+-(array of letter obsjs)
+[{},{},{},{}, {}]
+- each object represents a letter in the guess word 
+{key: "a", color: "yellow"}
+
+CURRENT GUESS
+-string "hello"
+
+KEYPAD LETTERS
+array of letter objects [{key: "a", color: "green"}, {}, {}, {}, {}]
+
+NUMBER OF TURNS
+integer between 0-6
+
+GAME PROCESS
+
+ENETRINGWORDS IN THE GRID
+- user enters a letter and a square fills that letter
+- when a user hits enter it submits the word
+-if all squares are not filled with letter then the word is not submitted
+- if that word has already been used in a prev guess then the word is not submitted
+
+CHECKING SUBMITTED WORDS (each turn)
+-each letter is checked to see if it matches the solution
+-each letter is assigned a color based if it is part of the solution
+  --exact matches (green)correct position
+  --partial matches (yellow) not in correct position
+  --no matches (not in the solution at all) (grey)
+  - the guess is added to th egrid with the correct colors
+  - the keypad letters are updated (colors)
+
+  ENDING GAME
+  -when the guessed word fully matches the solution ("well done")
+
+  runs out of guesses
+  ("oops! try again")
+
+  - 
+  JSON file, 3rd party api, own database served in mongodb
+*/
