8f5524144b58e1c6589186bd756293edb14c2b08
PaolaP <paolaperez8@gmail.com>
2022-07-10 12:21:59 -0400

    usState in hook for turn, currentGuess, guesses, history, isCorrect

--- src/hooks/useWords.js
+++ src/hooks/useWords.js
@@ -1,24 +1,26 @@
+import { useState } from "react";
+
 const useWords = (solution) => {
+  const [turn, setTurn] = useState(0);
+  const [currentGuess, setCurrentGuess] = useState("");// every time they hit a new key with handlekeyup
+  const [guesses, setGuesses] = useState([]); //each guess is an array -formatGuess includes color
+  const [history, setHistory] = useState([]); // each guess is a string, no duplicate guesses
+  const [isCorrect, setIsCorrect] = useState(false); //only true when user wins the game
 
-//format a new guess into an array of letter objects
-//eg. [{key: "a", color: "green"}]
-const formatGuess = () =>{
+  //format a new guess into an array of letter objects
+  //eg. [{key: "a", color: "green"}]
+  const formatGuess = () => {};
 
-}
+  //add a new guess to guesss state
+  //update the isCorrect state if the guess is correct
+  // add one to the turn state, keep track of turn
+  const addNewGuess = () => {};
 
-//add a new guess to guesss state
-//update the isCorrect state if the guess is correct
-// add one to the turn state, keep track of turn
-const addNewGuess = () => {
+  //handle keyup event & track current guess
+  //if user presses enter, add submits the new guess
+  const handleKeyup = () => {};
 
-}
-
-//handle keyup event & track current guess
-//if user presses enter, add submits the new guess
-const handleKeyup = () => {
-
-}
-
-}
+  return { turn, currentGuess, guesses, isCorrect, handleKeyup };
+};
 
 export default useWords;
