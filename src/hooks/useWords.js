1fbc33081c2e4b1c373a86f752f0e8cafe7f8862
PaolaP <paolaperez8@gmail.com>
2022-07-09 23:14:03 -0400

    use words hook, formatGuess, addNewGuess, handleKeyUp

---
+++ src/hooks/useWords.js
@@ -0,0 +1,24 @@
+const useWords = (solution) => {
+
+//format a new guess into an array of letter objects
+//eg. [{key: "a", color: "green"}]
+const formatGuess = () =>{
+
+}
+
+//add a new guess to guesss state
+//update the isCorrect state if the guess is correct
+// add one to the turn state
+const addNewGuess = () => {
+
+}
+
+//handle keyup event & track current guess
+//if user presses enter, add the new guess
+const handleKeyup = () => {
+
+}
+
+}
+
+export default useWords;
