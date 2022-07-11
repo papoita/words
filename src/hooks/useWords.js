7634ffdbfd612376229043f2ccb52dfd1f1d03f3
PaolaP <paolaperez8@gmail.com>
2022-07-11 15:52:44 -0400

    allow guesses no more than 5 chars, no more than 5 turns, not a repeat word and call formatGuess function

--- src/hooks/useWords.js
+++ src/hooks/useWords.js
@@ -4,12 +4,14 @@ const useWords = (solution) => {
   const [turn, setTurn] = useState(0);
   const [currentGuess, setCurrentGuess] = useState(""); // every time they hit a new key with handlekeyup
   const [guesses, setGuesses] = useState([]); //each guess is an array -formatGuess includes color
-  const [history, setHistory] = useState([]); // each guess is a string, no duplicate guesses
+  const [history, setHistory] = useState(["hello", "ninja"]); // each guess is a string, no duplicate guesses
   const [isCorrect, setIsCorrect] = useState(false); //only true when user wins the game
 
   //format a new guess into an array of letter objects
   //eg. [{key: "a", color: "green"}]
-  const formatGuess = () => {};
+  const formatGuess = () => {
+    console.log("formatting the guess:", currentGuess)
+  };
 
   //add a new guess to guesss state
   //update the isCorrect state if the guess is correct
@@ -20,11 +22,30 @@ const useWords = (solution) => {
   //if user presses enter, add submits the new guess
   const handleKeyup = ({ key }) => {
     // console.log(key) would record any key including delete and shift, so you want to have only letters a-z using regular expressions
+    if (key === "Enter") {
+      //only add if turn is less than 5
+      if (turn > 5) {
+        console.log("You used all your turns");
+        return;
+      }
+      //do not allow duplicate words
+      if (history.includes(currentGuess)) {
+        console.log("You already tried this word");
+        return;
+      }
+      //if words is  5 characters long
+      if (currentGuess.length !== 5) {
+        console.log("Word has to be 5 characters long");
+        return;
+      }
+      formatGuess();
+    }
+
     if (key === "Backspace") {
       setCurrentGuess((prev) => {
         return prev.slice(0, -1);
       });
-      return
+      return;
     }
     if (/^[A-Za-z]$/.test(key)) {
       if (currentGuess.length < 5) {
