6281bfb4cd9b90657d89bb923317cf6b47f6463d
PaolaP <paolaperez8@gmail.com>
2022-08-05 18:10:41 -0400

    add modal for game end

---
+++ src/components/Modal.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Modal ({isCorrect, turn, solution}) {
+return (
+  <div className="modal">
+    {isCorrect && (
+      <div>
+        <h1>
+          CONGRATS: you win!!!
+        </h1>
+        <p className="solution"> {solution}</p>
+        <p>You found the solution in {turn} guesses =)</p>
+      </div>
+    )}
+
+{!isCorrect && (
+      <div>
+        <h2>
+          Sorry, try again
+        </h2>
+        <p className="solution"> The solution was: <bold> {solution}</bold></p>
+        <p>You'll do better next time</p>
+      </div>
+    )}
+
+  </div>
+)
+}
--- src/components/Words.js
+++ src/components/Words.js
@@ -1,23 +1,25 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import useWords from "../hooks/useWords";
 import Grid from "./Grid";
 import Keypad from "./Keypad";
+import Modal from "./Modal";
 
 export default function Words({ solution }) {
   const { currentGuess, handleKeyup, guesses, isCorrect, turn, usedKeys } = useWords(solution);
+  const [showModal, setShowModal]= useState(false);
 
   useEffect(() => {
     window.addEventListener("keyup", handleKeyup);
 
     if(isCorrect){
       console.log("CONGRATS! you win")
-      alert("CONGRATS! you win!")
+      setTimeout(() => setShowModal(true), 2000);
       window.removeEventListener("keyup", handleKeyup);
     }
 
     if(turn > 5){
       console.log("Sorry, you are out of guesses")
-      alert("Sorry, you are out of guesses")
+      setTimeout(() => setShowModal(true), 2000);
       window.removeEventListener("keyup", handleKeyup);
     }
 
@@ -34,6 +36,7 @@ export default function Words({ solution }) {
       <div>currentGuess - {currentGuess}</div> */}
       <Grid currentGuess={currentGuess} guesses = {guesses} turn = {turn}/>
       <Keypad usedKeys = {usedKeys}/>
+      {showModal && <Modal isCorrect = {isCorrect} turn={turn} solution={solution}/> }
     </>
   );
 }
